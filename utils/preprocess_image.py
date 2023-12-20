54ee53168cffdf97ce351e435fd1da4624cf54a4
michaelgobz <raniahgobz@outlook.com>
2023-12-21 00:58:15 +0300

    opennig the image file the safe way

--- utils/preprocess_image.py
+++ utils/preprocess_image.py
@@ -10,7 +10,7 @@ import numpy as np
 from PIL import Image
 
 
-def process_image(image):
+def process_image(image_path):
 
     """
     function to preprocess the image for prediction it does the following:
@@ -26,25 +26,26 @@ def process_image(image):
     Returns: the image as a  float tensor
 
     """
-    image = Image.open(image)
-    image = image.resize((256, 256))
-    # crop the image
-    width, height = image.size
-    new_width = 224
-    new_height = 224
-    left = int((width - new_width) / 2)
-    top = int((height - new_height) / 2)
-    right = int((width + new_width) / 2)
-    bottom = int((height + new_height) / 2)
-    image = image.crop((left, top, right, bottom))
+    with Image.open(image_path) as image:
+        # resize the image
+        image = image.resize((256, 256))
+        # crop the image
+        width, height = image.size
+        new_width = 224
+        new_height = 224
+        left = int((width - new_width) / 2)
+        top = int((height - new_height) / 2)
+        right = int((width + new_width) / 2)
+        bottom = int((height + new_height) / 2)
+        image = image.crop((left, top, right, bottom))
 
-    # convert to a numpy array
-    image = np.array(image)
-    image = image / 255
-    # normalize the image with the mean and std
-    image = (image - [0.485, 0.456, 0.406]) / [0.229, 0.224, 0.225]
-    image = image.transpose((2, 0, 1))
-    image = torch.from_numpy(image)
-    image = image.float()
+        # convert to a numpy array
+        image = np.array(image)
+        image = image / 255
+        # normalize the image with the mean and std
+        image = (image - [0.485, 0.456, 0.406]) / [0.229, 0.224, 0.225]
+        image = image.transpose((2, 0, 1))
+        image = torch.from_numpy(image)
+        image = image.float()
     
     return image
